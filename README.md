a90b552b1468a4b2fed61e7feb8aa956ff4b1937
Nisheeth Barthwal <nisheeth.barthwal@moia.io>
2020-07-26 15:06:04 +0200

    implement push to coveralls, add readme

---
+++ README.md
@@ -0,0 +1,20 @@
+# Coveralls Jacoco Kotlin Gradle Plugin
+A jacoco test coverage reporter gradle plugin for Kotlin and [coveralls.io](https://coveralls.io).
+
+The plugin supports non-root packages in line with the recommended [Kotlin directory structure](https://kotlinlang.org/docs/reference/coding-conventions.html#directory-structure) 
+which was missing in many other plugins for the Kotlin ecosystem. 
+
+## Usage
+Apply the plugin with the ID: `com.github.nbaztec.coveralls-jacoco-kotlin`
+
+This will add a gradle task `coverallsJacoco` that can be used to publish coverage report via `./gradlew test coverallsJacoco`
+
+## Options
+```kotlin
+coverallsJacoco {
+    dependsOn test
+    
+    rootPackage = 'com.github.nbaztec.foo' // optional, leave out if project has java directory structure  
+    reportPath = 'build/reports/jacoco/test/jacocoTestReport.xml'
+}
+```
---
+++ src/main/kotlin/ServiceInfoParser.kt
@@ -0,0 +1,58 @@
+package org.gradle.plugin.coveralls.jacoco
+
+data class ServiceInfo(val name: String, val jobId: String? = null, val pr: String? = null, val branch: String? = null)
+
+class ServiceInfoParser {
+    companion object {
+        private val isJenkins = System.getenv("JENKINS_URL") != null
+        private val isTravis = System.getenv("TRAVIS") == "true"
+        private val isCircleCI = System.getenv("CIRCLECI") == "true"
+        private val isCodeship = System.getenv("CI_NAME") == "codeship"
+        private val isGithubActions = System.getenv("GITHUB_ACTIONS") != null
+
+        private fun envOrNull(v: String): String? = System.getenv(v).takeIf(String::isNotBlank)
+
+        fun parse(): ServiceInfo {
+            return when {
+                isJenkins -> ServiceInfo(
+                        "jenkins",
+                        envOrNull("BUILD_NUMBER"),
+                        envOrNull("ghprbPullId"),
+                        envOrNull("GIT_BRANCH")
+                )
+                isTravis -> ServiceInfo(
+                        envOrNull("CI_NAME") ?: "travis-pro",
+                        envOrNull("TRAVIS_JOB_ID"),
+                        envOrNull("TRAVIS_PULL_REQUEST"),
+                        envOrNull("TRAVIS_BRANCH")
+                )
+                isCircleCI -> {
+                    ServiceInfo(
+                            "circleci",
+                            envOrNull("CIRCLE_BUILD_NUM"),
+                            envOrNull("CIRCLE_PULL_REQUEST")?.substringAfterLast("/"),
+                            envOrNull("CIRCLE_BRANCH")
+                    )
+                }
+                isCodeship -> ServiceInfo(
+                        "codeship",
+                        envOrNull("CI_BUILD_NUMBER"),
+                        envOrNull("CI_PR_NUMBER"),
+                        envOrNull("CI_BRANCH")
+                )
+                isGithubActions -> ServiceInfo(
+                        "github-actions",
+                        envOrNull("BUILD_NUMBER"),
+                        envOrNull("GITHUB_REF")?.let { ref ->
+                            "refs/pull/(\\d+)/merge".toRegex().find(ref)?.let {
+                                it.groupValues[1]
+                            }
+                        },
+                        envOrNull("CI_BRANCH")
+                )
+                else -> ServiceInfo("other")
+            }
+        }
+    }
+}
+
