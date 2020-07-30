124e502acbe548fb512642fa15e915a31941032a
Nisheeth Barthwal <nisheeth.barthwal@gmail.com>
2020-07-30 17:52:05 +0200

    update .git dir in test

--- src/test/kotlin/GitInfoParserTest.kt
+++ src/test/kotlin/GitInfoParserTest.kt
@@ -1,5 +1,6 @@
 package org.gradle.plugin.coveralls.jacoco
 
+import org.gradle.internal.impldep.com.google.common.io.Files
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import java.io.File
@@ -9,7 +10,11 @@ internal class GitInfoParserTest {
 
     @Test
     fun `GitInfoParser parses git repo details`() {
+        Files.move(File(testRepo, "git"), File(testRepo, ".git"))
+
         val actual = GitInfoParser.parse(testRepo)
+        Files.move(File(testRepo, ".git"), File(testRepo, "git"))
+
         val expected = GitInfo(
                 Head(
                         "4cd72eadcc34861139b338dd859344d419244e0b",
