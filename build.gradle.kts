c98ba0fa86bdd78deca64496f6c6363652f10d09
Nisheeth Barthwal <nisheeth.barthwal@gmail.com>
2020-09-06 14:30:41 +0200

    add compile only dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 group = "com.github.nbaztec"
-version = "1.2.0"
+version = "1.2.1"
 
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = JavaVersion.VERSION_1_8.toString()
@@ -10,14 +10,6 @@ tasks.withType<KotlinCompile> {
 java {
     sourceCompatibility = JavaVersion.VERSION_1_8
     targetCompatibility = JavaVersion.VERSION_1_8
-}
-
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-        jcenter()
-    }
 }
 
 repositories {
@@ -33,7 +25,7 @@ dependencies {
     implementation("org.eclipse.jgit", "org.eclipse.jgit", "5.8.1.202007141445-r")
     implementation("org.apache.httpcomponents", "httpmime", "4.5.12")
     implementation("com.google.code.gson", "gson", "2.8.5")
-    implementation("com.android.tools.build", "gradle", "4.0.1")
+    compileOnly("com.android.tools.build", "gradle", "4.0.1")
     testImplementation("junit", "junit", "4.13")
     testImplementation("org.junit.jupiter", "junit-jupiter-api", "5.6.2")
     testRuntimeOnly("org.junit.jupiter", "junit-jupiter-engine", "5.6.2")
@@ -72,7 +64,7 @@ plugins {
     `maven-publish`
     id("org.jetbrains.kotlin.jvm") version "1.3.72"
     id("com.gradle.plugin-publish") version "0.12.0"
-    id("com.github.nbaztec.coveralls-jacoco") version "1.2.0"
+    id("com.github.nbaztec.coveralls-jacoco") version "1.1.2"
 }
 
 publishing {
