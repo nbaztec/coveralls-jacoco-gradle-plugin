1a4e9e519ba452bc8a8a54da4633ddf3233d188f
Nisheeth Barthwal <nisheeth.barthwal@gmail.com>
2020-09-06 14:12:30 +0200

    bump minor version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 group = "com.github.nbaztec"
-version = "1.1.3"
+version = "1.2.0"
 
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = JavaVersion.VERSION_1_8.toString()
