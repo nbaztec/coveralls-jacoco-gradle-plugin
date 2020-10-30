b9d4affd723c4b0461002c71c638a82e263e6404
Nisheeth Barthwal <nisheeth.barthwal@gmail.com>
2020-10-30 17:42:50 +0100

    use latest version

--- build.gradle.kts
+++ build.gradle.kts
@@ -72,7 +72,7 @@ plugins {
     `maven-publish`
     id("org.jetbrains.kotlin.jvm") version "1.3.72"
     id("com.gradle.plugin-publish") version "0.12.0"
-    id("com.github.nbaztec.coveralls-jacoco") version "1.2.2"
+    id("com.github.nbaztec.coveralls-jacoco") version "1.2.4"
 }
 
 publishing {
