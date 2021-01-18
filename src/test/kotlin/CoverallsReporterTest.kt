5971162e5455311b00f5f04cc7596083f136e473
Nisheeth Barthwal <nisheeth.barthwal@gmail.com>
2021-01-18 17:59:52 +0100

    update tests

--- src/test/kotlin/CoverallsReporterTest.kt
+++ src/test/kotlin/CoverallsReporterTest.kt
@@ -1,9 +1,12 @@
 package org.gradle.plugin.coveralls.jacoco
 
 import com.android.build.gradle.internal.dsl.BaseAppModuleExtension
+import com.google.gson.Gson
 import com.sun.net.httpserver.HttpServer
 import io.mockk.every
+import io.mockk.just
 import io.mockk.mockk
+import io.mockk.runs
 import io.mockk.spyk
 import io.mockk.verifyAll
 import kotlinx.coroutines.GlobalScope
@@ -13,8 +16,13 @@ import kotlinx.coroutines.runBlocking
 import org.gradle.api.Project
 import org.gradle.api.tasks.SourceSetContainer
 import org.gradle.internal.impldep.com.google.common.io.Files
-import org.junit.jupiter.api.*
+import org.junit.jupiter.api.AfterAll
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertNotEquals
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+import org.junit.jupiter.api.assertThrows
 import java.io.File
 import java.io.PrintWriter
 import java.net.InetSocketAddress
@@ -66,6 +74,8 @@ internal class CoverallsReporterTest {
 
         val project = mockProject { _, pluginExtension ->
             every { pluginExtension.apiEndpoint } returns "http://${mockServer.address.hostName}:${mockServer.address.port}"
+            every { pluginExtension.coverallsRequest } returns null
+            every { pluginExtension.dryRun } returns false
         }
 
         delay(50)
@@ -83,15 +93,62 @@ internal class CoverallsReporterTest {
     }
 
     @Test
+    fun `CoverallsReporter parses info and writes coveralls request to a file when option set`() = runBlocking {
+        val envGetter = createEnvGetter(
+            mapOf(
+                "GITHUB_TOKEN" to "test-token",
+                "GITHUB_ACTIONS" to "true"
+            )
+        )
+
+        val coverallsRequest = File.createTempFile("test", ".json").also { it.deleteOnExit() }
+        val project = mockProject { _, pluginExtension ->
+            every { pluginExtension.coverallsRequest } returns coverallsRequest
+            every { pluginExtension.dryRun } returns true
+        }
+
+        val reporter = spyk(CoverallsReporter(envGetter), recordPrivateCalls = true)
+        reporter.report(project)
+
+        assertEquals(546, coverallsRequest.length())
+    }
+
+    @Test
+    fun `CoverallsReporter parses info but skips sending to coveralls when dry run is true`() = runBlocking {
+        val envGetter = createEnvGetter(
+            mapOf(
+                "GITHUB_TOKEN" to "test-token",
+                "GITHUB_ACTIONS" to "true"
+            )
+        )
+
+        val coverallsRequest = File.createTempFile("test", ".json").also { it.deleteOnExit() }
+        val project = mockProject { _, pluginExtension ->
+            every { pluginExtension.apiEndpoint } returns "http://foobar"
+            every { pluginExtension.coverallsRequest } returns coverallsRequest
+            every { pluginExtension.dryRun } returns true
+        }
+
+        val reporter = spyk(CoverallsReporter(envGetter), recordPrivateCalls = true)
+        reporter.report(project)
+
+        verifyAll(inverse = true) {
+            reporter["send"](any<String>(), any<Request>())
+        }
+
+        assertNotEquals(0, coverallsRequest.length())
+    }
+
+    @Test
     fun `CoverallsReporter parses info and sends report to coveralls server when GITHUB_TOKEN is set`() = runBlocking {
         var actual = ""
         val mockServer = HttpServer.create(InetSocketAddress("localhost", 0), 0).apply {
             createContext("/") { http ->
                 actual = http.requestBody.reader().readText()
-                        .trim()
-                        .split("\r\n")
-                        .drop(1).dropLast(1)
-                        .joinToString("\n")
+                    .trim()
+                    .split("\r\n")
+                    .drop(1).dropLast(1)
+                    .joinToString("\n")
 
                 http.sendResponseHeaders(200, 0)
                 http.close()
@@ -101,12 +158,16 @@ internal class CoverallsReporterTest {
             mockServer.start()
         }
 
-        val envGetter = createEnvGetter(mapOf(
+        val envGetter = createEnvGetter(
+            mapOf(
                 "GITHUB_TOKEN" to "test-token",
                 "GITHUB_ACTIONS" to "true"
-        ))
+            )
+        )
         val project = mockProject { _, pluginExtension ->
             every { pluginExtension.apiEndpoint } returns "http://${mockServer.address.hostName}:${mockServer.address.port}"
+            every { pluginExtension.coverallsRequest } returns null
+            every { pluginExtension.dryRun } returns false
         }
 
         delay(50)
@@ -118,7 +179,7 @@ internal class CoverallsReporterTest {
 Content-Type: application/json; charset=UTF-8
 Content-Transfer-Encoding: binary
 
-{"repo_token":"test-token","service_name":"github","git":{"head":{"id":"4cd72eadcc34861139b338dd859344d419244e0b","author_name":"John Doe","author_email":"test@example.com","committer_name":"John Doe","committer_email":"test@example.com","message":"test commit\n"},"branch":"master","remotes":[{"name":"origin","url":"git@github.com:test/testrepo.git"}]},"source_files":[{"name":"javaStyleSrc/main/kotlin/foo/bar/baz/Main.kt","source_digest":"36083cd4c2ac736f9210fd3ed23504b5","coverage":[null,null,null,null,1,1,1,1,null,1,1,0,0,1,1,null,1,1,1]}]}
+{"repo_token":"test-token","service_name":"github","git":{"head":{"id":"4cd72eadcc34861139b338dd859344d419244e0b","author_name":"John Doe","author_email":"test@example.com","committer_name":"John Doe","committer_email":"test@example.com","message":"test commit"},"branch":"master","remotes":[{"name":"origin","url":"git@github.com:test/testrepo.git"}]},"source_files":[{"name":"javaStyleSrc/main/kotlin/foo/bar/baz/Main.kt","source_digest":"36083cd4c2ac736f9210fd3ed23504b5","coverage":[null,null,null,null,1,1,1,1,null,1,1,0,0,1,1,null,1,1,1]}]}
         """.trimIndent()
 
         assertEquals(expected, actual)
@@ -130,10 +191,10 @@ Content-Transfer-Encoding: binary
         val mockServer = HttpServer.create(InetSocketAddress("localhost", 0), 0).apply {
             createContext("/") { http ->
                 actual = http.requestBody.reader().readText()
-                        .trim()
-                        .split("\r\n")
-                        .drop(1).dropLast(1)
-                        .joinToString("\n")
+                    .trim()
+                    .split("\r\n")
+                    .drop(1).dropLast(1)
+                    .joinToString("\n")
 
                 http.sendResponseHeaders(200, 0)
                 http.close()
@@ -146,6 +207,8 @@ Content-Transfer-Encoding: binary
         val envGetter = createEnvGetter(mapOf("COVERALLS_REPO_TOKEN" to "test-token"))
         val project = mockProject { _, pluginExtension ->
             every { pluginExtension.apiEndpoint } returns "http://${mockServer.address.hostName}:${mockServer.address.port}"
+            every { pluginExtension.coverallsRequest } returns null
+            every { pluginExtension.dryRun } returns false
         }
 
         delay(50)
@@ -157,7 +220,7 @@ Content-Transfer-Encoding: binary
 Content-Type: application/json; charset=UTF-8
 Content-Transfer-Encoding: binary
 
-{"repo_token":"test-token","service_name":"other","git":{"head":{"id":"4cd72eadcc34861139b338dd859344d419244e0b","author_name":"John Doe","author_email":"test@example.com","committer_name":"John Doe","committer_email":"test@example.com","message":"test commit\n"},"branch":"master","remotes":[{"name":"origin","url":"git@github.com:test/testrepo.git"}]},"source_files":[{"name":"javaStyleSrc/main/kotlin/foo/bar/baz/Main.kt","source_digest":"36083cd4c2ac736f9210fd3ed23504b5","coverage":[null,null,null,null,1,1,1,1,null,1,1,0,0,1,1,null,1,1,1]}]}
+{"repo_token":"test-token","service_name":"other","git":{"head":{"id":"4cd72eadcc34861139b338dd859344d419244e0b","author_name":"John Doe","author_email":"test@example.com","committer_name":"John Doe","committer_email":"test@example.com","message":"test commit"},"branch":"master","remotes":[{"name":"origin","url":"git@github.com:test/testrepo.git"}]},"source_files":[{"name":"javaStyleSrc/main/kotlin/foo/bar/baz/Main.kt","source_digest":"36083cd4c2ac736f9210fd3ed23504b5","coverage":[null,null,null,null,1,1,1,1,null,1,1,0,0,1,1,null,1,1,1]}]}
         """.trimIndent()
 
         assertEquals(expected, actual)
