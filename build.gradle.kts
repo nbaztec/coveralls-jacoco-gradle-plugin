2bcb78f71811aa3500e5495407049e6c3831c110
Nisheeth Barthwal <nisheeth.barthwal@gmail.com>
2021-01-24 14:44:15 +0100

    fix test report path

--- build.gradle.kts
+++ build.gradle.kts
@@ -92,7 +92,7 @@ tasks {
             xml.isEnabled = true
             html.isEnabled = true
         }
-        executionData(testAndroid)
+        executionData(File(buildDir, "jacoco/testAndroid.exec"))
     }
 
     create("setupPublishSecrets") {
