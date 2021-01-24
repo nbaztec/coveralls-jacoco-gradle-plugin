dc88618f0cfaa295581c67b306ec70dea03b7214
Nisheeth Barthwal <nisheeth.barthwal@gmail.com>
2021-01-24 18:08:28 +0100

    downgrade dom4j (#36)
    * downgrade dom4j
    
    * downgrade plugin version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 group = "com.github.nbaztec"
-version = "1.2.9"
+version = "1.2.10"
 
 buildscript {
     repositories {
@@ -49,7 +49,7 @@ val testAndroidMainRuntimeOnly: Configuration by configurations.getting {
 dependencies {
     implementation("org.jetbrains.kotlinx", "kotlinx-coroutines-core", "1.4.2")
     implementation("org.jetbrains.kotlin", "kotlin-reflect", "1.4.21")
-    implementation("org.dom4j", "dom4j", "2.1.3")
+    implementation("org.dom4j", "dom4j", "2.1.1")
     implementation("org.jetbrains.kotlin", "kotlin-gradle-plugin", "1.4.21")
     implementation("org.eclipse.jgit", "org.eclipse.jgit", "5.10.0.202012080955-r")
     implementation("org.apache.httpcomponents", "httpmime", "4.5.13")
@@ -116,7 +116,7 @@ plugins {
     `maven-publish`
     id("org.jetbrains.kotlin.jvm") version "1.4.21"
     id("com.gradle.plugin-publish") version "0.12.0"
-    id("com.github.nbaztec.coveralls-jacoco") version "1.2.9"
+    id("com.github.nbaztec.coveralls-jacoco") version "1.2.8"
 }
 
 publishing {
