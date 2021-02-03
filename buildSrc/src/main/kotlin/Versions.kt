c9edc8240c40fcf8e6c5304bfedd59d95429d20b
Nisheeth Barthwal <nisheeth.barthwal@gmail.com>
2021-02-03 18:28:42 +0100

    add buildSrc

---
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -0,0 +1,17 @@
+object Versions {
+    const val kotlin = "1.4.21"
+    const val kotlinxCoroutines = "1.4.2"
+
+    const val dom4j = "2.1.1"
+    const val jgit = "5.10.0.202012080955-r"
+    const val httpMime = "4.5.13"
+    const val gson = "2.8.6"
+    const val androidBuildTools = "4.0.1"
+
+    const val junit = "4.13.1"
+    const val jupiter = "5.7.0"
+    const val mockk = "1.10.5"
+
+    const val gradlePublishPlugin = "0.12.0"
+    const val gradleCoverallsJacocoPlugin = "1.2.11"
+}
