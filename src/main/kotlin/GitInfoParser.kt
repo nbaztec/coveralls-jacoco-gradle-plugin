5bdf9b7ef594832a2f540b6dbac4396f705a6b0d
Nisheeth Barthwal <nisheeth.barthwal@gmail.com>
2021-02-03 16:30:13 +0100

    sample commit msg

--- src/main/kotlin/GitInfoParser.kt
+++ src/main/kotlin/GitInfoParser.kt
@@ -36,6 +36,8 @@ object GitInfoParser {
                 val rev = repo.resolve("HEAD")
                 val commit = RevWalk(repo).parseCommit(rev)
 
+                logger.debug("commit message: ${commit.fullMessage.trim()}")
+
                 Head(
                         rev.name,
                         commit.authorIdent.name,
