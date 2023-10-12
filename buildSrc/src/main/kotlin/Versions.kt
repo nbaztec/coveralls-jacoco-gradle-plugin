fce2893f5727d0d596f8fa032bad0e3db22c45ef
Nisheeth Barthwal <nbaztec@gmail.com>
2023-10-12 11:21:20 +0200

    update dom4j and gson deps

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -2,10 +2,10 @@ object Versions {
     const val kotlin = "1.8.22"
     const val kotlinxCoroutines = "1.4.2"
 
-    const val dom4j = "2.1.1"
+    const val dom4j = "2.1.4"
     const val jgit = "5.10.0.202012080955-r"
     const val httpMime = "4.5.13"
-    const val gson = "2.8.6"
+    const val gson = "2.10.1"
     const val androidBuildTools = "4.0.1"
 
     const val junit = "4.13.1"
